f67ddbb8c26ba4c2c4ec3ab52781470dedac50ed
Aravindhan <aravindhanraj747@gmail.com>
2022-09-29 16:10:57 +0530

    Updating style

--- src/app/components/adminhome/adminhome.component.ts
+++ src/app/components/adminhome/adminhome.component.ts
@@ -1,4 +1,7 @@
+import { BLACK_ON_WHITE_CSS_CLASS } from '@angular/cdk/a11y/high-contrast-mode/high-contrast-mode-detector';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-adminhome',
@@ -6,14 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adminhome.component.css']
 })
 export class AdminhomeComponent implements OnInit {
-
-  typeList:any[]=["Buy","Rent","Land","Commericial","Lease","PG"]
-  constructor() { }
+  
+  typeList:any[] = ["Buy","Rent","Land","Commericial","Lease","PG"]
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
   }
 
   choosephoto(event:any){
-
   }
+  logout(){
+    this.route.navigate(['adminlogin']);
+  }
+
 }
